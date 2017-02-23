31d7215f13cbe55bd4dca219ca4851f85d8925fc
Stenukhin Alexey <aasten@gmail.com>
2017-02-23 15:20:22 +0300

    working test transmission

--- src/by/ishop/RESTMain.java
+++ src/by/ishop/RESTMain.java
@@ -10,12 +10,14 @@ import by.ishop.data.product.ObjectFactory;
 public class RESTMain {
 
 
+    final String test = "{\"name\":\"Canon EOS 5D\",\"imgURL\":\"/img/canon.png\",\"price\":2000,\"characteristics\":[{\"propName\":\"prop name 1\",\"propValue\":\"prop value 1\"},{\"propName\":\"prop name 2\",\"propValue\":\"prop value 2\"}]}";
+
     @GET
     @Path("entry/{id}")
     @Produces("application/json")
     public Response getEntry(@PathParam("id") String id) {
         try {
-            return Response.status(200).entity(ObjectFactory.createProductEntry()).build();
+            return Response.status(200).entity(test).build();
         } catch(RuntimeException e) {
             return Response.status(404).entity("Entry with id " + id + " is not found").build();
         }
