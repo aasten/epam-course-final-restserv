46ccc4a7c838cf116e07abf3824dbe59f33d4ee1
Stenukhin Alexey <aasten@gmail.com>
2017-03-01 19:28:47 +0300

    moving java sources to src/main/java

--- src/main/java/by/ishop/data/DataAccess.java
+++ src/main/java/by/ishop/data/DataAccess.java
@@ -4,7 +4,7 @@ import by.ishop.data.product.ProductEntry;
 public enum DataAccess {
     INSTANCE;
 
-    private DAO<ProductEntry, Long> entryDAO = new JDBCDAOProductEntry();
+    private DAO<ProductEntry, Long> entryDAO = new JPADAOProductEntry();
 
     public DAO<ProductEntry, Long> getEntryDAO() {
         return entryDAO;
